c32e5a36b9a9912b71c75b1ec55ca72f36301c68
Nataliya Shipelova <n.shipelova@butik.ru>
2019-07-02 17:35:46 +0300

    feat (list of employees): render data

--- src/components/List.js
+++ src/components/List.js
@@ -14,6 +14,25 @@ class List extends React.PureComponent {
     return (
       <div className='list'>
         <h2>Список сотрудников</h2>
+        { this.props.list.length
+          ? <div className='list__table'>
+            <div className='list__table-row'>
+              <div className='list__table-col list__table_header'>Имя</div>
+              <div className='list__table-col list__table_header'>Фамилия</div>
+              <div className='list__table-col list__table_header'>Должность</div>
+              <div className='list__table-col list__table_header'>Описание</div>
+            </div>
+            {this.props.list.map(item =>
+              <div key={item.id} className='list__table-row'>
+                <div className='list__table-col'>{item.name}</div>
+                <div className='list__table-col'>{item.surname}</div>
+                <div className='list__table-col'>{item.position}</div>
+                <div className='list__table-col'>{item.description}</div>
+              </div>
+            )}
+          </div>
+          : <div>Список сотрудников пуст</div>
+        }
       </div>
     );
   }
--- src/employees.js
+++ src/employees.js
@@ -1,23 +1,27 @@
 export const getEmployees = () => [
   {
+    id: 1,
     name: 'Иван',
     surname: 'Иванов',
     position: 'Программист',
     description: 'Back-End разработчик',
   },
   {
+    id: 2,
     name: 'Антон',
     surname: 'Петров',
     position: 'Программист',
     description: 'Front-End разработчик',
   },
   {
+    id: 3,
     name: 'Светлана',
     surname: 'Сидорова',
     position: 'Дизайнер',
     description: 'Дизайнер интерфейсов',
   },
   {
+    id: 4,
     name: 'Андрей',
     surname: 'Попов',
     position: 'Менеджер',
